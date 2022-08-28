8a676ee978822516e27b83df6622a49c4a982717
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2022-08-28 14:46:19 +0200

    refactor all the buttons

--- lib/add_tower/add_tower.dart
+++ lib/add_tower/add_tower.dart
@@ -1,16 +1,22 @@
 // Package imports:
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 
 // Project imports:
-import 'package:flame_test/add_tower/add_tower_button.dart';
-import 'package:flame_test/add_tower/place_tower_button.dart';
+import 'package:flame_test/add_tower/text_button.dart';
+import 'package:flame_test/main.dart';
 import 'package:flame_test/tower.dart';
 
-class AddTowerComponent extends PositionComponent {
-  final AddTowerButton addTowerButton = AddTowerButton()
-    ..position = Vector2(0, 0);
-  final PlaceTowerButton placeTowerButton = PlaceTowerButton()
-    ..position = Vector2(0, 60);
+class AddTowerComponent extends PositionComponent
+    with HasGameRef<SpaceShooterGame> {
+  final TextButtonComponent addTowerButton =
+      TextButtonComponent(text: "Add Tower")..position = Vector2(0, 0);
+
+  final TextButtonComponent placeTowerButton =
+      TextButtonComponent(text: "Place Tower")..position = Vector2(0, 60);
+
+  final TextButtonComponent abortPlaceTowerButton =
+      TextButtonComponent(text: "Abort")..position = Vector2(0, 120);
 
   Tower? newTower;
 
@@ -18,17 +24,54 @@ class AddTowerComponent extends PositionComponent {
   Future<void> onLoad() async {
     await super.onLoad();
 
+    addTowerButton.onPressed = onAddTower;
+    placeTowerButton.onPressed = onPlaceTower;
+    abortPlaceTowerButton.onPressed = onAbortPlaceTower;
+
     add(addTowerButton);
   }
 
-  @override
-  void update(double dt) {
-    super.update(dt);
+  void onAddTower() {
+    if (!placeTowerButton.isMounted && !abortPlaceTowerButton.isMounted) {
+      newTower = Tower(
+        firingRange: 200,
+        acquisitionRange: 250,
+      )..position = Vector2(200, 200);
+
+      gameRef.add(newTower!);
+
+      add(placeTowerButton);
+      add(abortPlaceTowerButton);
+    }
   }
 
-  void addPlaceTowerButton() {
-    if (newTower != null && !placeTowerButton.isMounted) {
-      add(placeTowerButton);
+  void onPlaceTower() {
+    if (newTower != null &&
+        placeTowerButton.isMounted &&
+        abortPlaceTowerButton.isMounted) {
+      newTower!.placed = true;
+      newTower = null;
+
+      placeTowerButton.onTapCancel();
+      placeTowerButton.add(RemoveEffect());
+
+      abortPlaceTowerButton.onTapCancel();
+      abortPlaceTowerButton.add(RemoveEffect());
+    }
+  }
+
+  void onAbortPlaceTower() {
+    if (newTower != null &&
+        placeTowerButton.isMounted &&
+        abortPlaceTowerButton.isMounted) {
+      newTower?.add(RemoveEffect());
+      newTower = null;
+
+      placeTowerButton.onTapCancel();
+      placeTowerButton.add(RemoveEffect());
+
+      abortPlaceTowerButton.onTapCancel();
+      abortPlaceTowerButton.add(RemoveEffect());
     }
   }
 }
